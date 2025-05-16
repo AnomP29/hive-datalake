68266f8a807a4223c79e56af9e30a66848e11fa2
anom <anom.prasetyo@sinbad.co.id>
2025-05-16 15:22:22 +0700

    external-table

--- spark/python_script/create-hive-external-table.py
+++ spark/python_script/create-hive-external-table.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import TimestampType
+from pyspark.sql.types import *
+from py4j.java_gateway import java_import
+from functools import reduce
+import re
+
 
 # Initialize Spark with Hive support
 # spark = SparkSession.builder \
@@ -24,22 +28,92 @@ builder = builder \
 
 spark = builder.getOrCreate()
 
-
-
 spark.conf.set("hive.exec.dynamic.partition", "true")
 spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
 
+canonical_schema = StructType([
+    StructField("VendorID", LongType()),
+    StructField("tpep_pickup_datetime", TimestampType()),
+    StructField("tpep_dropoff_datetime", TimestampType()),
+    StructField("passenger_count", DoubleType()),
+    StructField("trip_distance", DoubleType()),
+    StructField("RatecodeID", DoubleType()),
+    StructField("store_and_fwd_flag", StringType()),
+    StructField("PULocationID", LongType()),
+    StructField("DOLocationID", LongType()),
+    StructField("payment_type", LongType()),
+    StructField("fare_amount", DoubleType()),
+    StructField("extra", DoubleType()),
+    StructField("mta_tax", DoubleType()),
+    StructField("tip_amount", DoubleType()),
+    StructField("tolls_amount", DoubleType()),
+    StructField("improvement_surcharge", DoubleType()),
+    StructField("total_amount", DoubleType()),
+    StructField("congestion_surcharge", DoubleType()),
+    StructField("airport_fee", DoubleType(), nullable=True),
+    StructField("cbd_congestion_fee", DoubleType(), nullable=True),
+])
+
+# Load all file paths using Spark
+all_files_df = spark.read.format("binaryFile").load("s3a://nyc-taxi/raw/")
+print('Get file bucket')
+# Filter only Parquet files starting with yellow_tripdata_YYYY-MM.parquet
+parquet_files = (
+    all_files_df
+    .filter(col("path").rlike(r"yellow_tripdata_\d{4}-\d{2}\.parquet$"))
+    .select("path")
+    .rdd.flatMap(lambda x: x)
+    .collect()
+)
+
+dfs = []
+
+for file_path in parquet_files:
+    try:
+        df_part = spark.read.parquet(file_path)
+        for field in canonical_schema.fields:
+            if field.name in df_part.columns:
+                df_part = df_part.withColumn(field.name, col(field.name).cast(field.dataType))
+            else:
+                df_part = df_part.withColumn(field.name, lit(None).cast(field.dataType))
+        dfs.append(df_part.select([f.name for f in canonical_schema.fields]))
+    except Exception as e:
+        print(f"Skipping {file_path}: {e}")
 # Path to input parquet files on MinIO
-input_path = "s3a://nyc-taxi/raw/yellow_tripdata_*.parquet"
+# input_path = "s3a://nyc-taxi/raw/yellow_tripdata_*.parquet"
+
+# Read, Cast, and Union
+print('Read, Cast, and Union')
+from functools import reduce
+
+       
+df = reduce(lambda a, b: a.unionByName(b), dfs)
 
 # Path to output partitioned parquet folder on MinIO
 output_path = "s3a://nyc-taxi/trip-data/yellow/"
 
+
 # Read Parquet files
-df = spark.read.parquet(input_path)
+# df = spark.read \
+#     .option("mergeSchema", "true") \
+#     .schema(canonical_schema) \
+#     .parquet(input_path)
+
+# expected_columns = [f.name for f in canonical_schema]
+# for col_name in df.columns:
+#     if col_name.lower() in [x.lower() for x in expected_columns] and col_name not in expected_columns:
+#         df = df.withColumnRenamed(col_name, col_name.lower())
 
 # Determine pickup datetime column
 pickup_col = "tpep_pickup_datetime" if "tpep_pickup_datetime" in df.columns else df.columns[0]
+
+# def align_schema(df, expected_schema):
+#     for col_name, expected_type in expected_schema.items():
+#         if col_name in df.columns:
+#             df = df.withColumn(col_name, col(col_name).cast(expected_type))
+#     return df
+
+# df = align_schema(df, expected_schema)
 
 # Cast TimestampNTZ columns to Timestamp (if any)
 for field in df.schema.fields:
@@ -90,7 +164,9 @@ spark.sql(create_table_sql)
 table_schema = spark.table("nyc_taxi_trip_yellow").schema
 ordered_columns = [f.name for f in table_schema if f.name in df_partitioned.columns]
 df_ordered = df_partitioned.select(*ordered_columns)
+df_orderedf = df_ordered.withColumn("VendorID", col("VendorID").cast("bigint"))
 
+# spark.sql("MSCK REPAIR TABLE nyc_taxi_trip_yellow")
 
 try:
     print("Writing data into Hive table...")
@@ -106,6 +182,6 @@ spark.sql("SHOW TABLES").show()
 # spark.sql("MSCK REPAIR TABLE nyc_taxi_trip_yellow")
 
 print("Listing partitions...")
-# spark.sql("SHOW PARTITIONS nyc_taxi_trip_yellow").show(truncate=False)
+spark.sql("SHOW PARTITIONS nyc_taxi_trip_yellow").show(truncate=False)
 
 spark.stop()
